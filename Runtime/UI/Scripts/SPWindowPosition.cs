7a29efee001bd58c69a676fff88e3e87ac431cdc
Neilson K-S <neilson.ks@gmail.com>
2023-07-07 13:48:10 -0700

    window position offset types

--- Runtime/UI/Scripts/SPWindowPosition.cs
+++ Runtime/UI/Scripts/SPWindowPosition.cs
@@ -2,13 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum OffsetType{Camera, World, Local}
 public class SPWindowPosition : MonoBehaviour
 {
     [SerializeField] protected Transform follow;
     [SerializeField] protected SPWindow window;
+    [SerializeField] protected Vector3 offset = Vector3.zero;
+    [SerializeField] protected OffsetType offsetSpace = OffsetType.Camera; 
+
+    RectTransform rect;
 
     void Start() {
-        window = GetComponent<SPWindow>();
+        if(window == null) {
+            window = GetComponent<SPWindow>();
+        }
+
+        if(window == null) {
+            rect = GetComponent<RectTransform>();
+        } else {
+            rect = window.Rect;
+        }
         SetFollow(follow);
     }
 
@@ -26,6 +39,12 @@ public class SPWindowPosition : MonoBehaviour
     }
 
     void UpdatePosition() {
-        SPUIBase.WorldToCanvas(follow.position, window.Rect);
+        if(offsetSpace == OffsetType.Camera) {
+            SPUIBase.WorldToCanvas(follow.position + SPUIBase.Camera.transform.TransformPoint(offset) - SPUIBase.Camera.transform.position, rect);
+        } else if(offsetSpace == OffsetType.World) {
+            SPUIBase.WorldToCanvas(follow.position + offset, rect);
+        } else if(offsetSpace == OffsetType.Local) {
+            SPUIBase.WorldToCanvas(follow.InverseTransformPoint(offset), rect);
+        }
     }
 }
