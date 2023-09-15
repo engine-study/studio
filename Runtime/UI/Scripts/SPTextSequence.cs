6ef47ed134fd42b7ff8e62eacd0c24848ee343ed
Neilson <neilson.ks@gmail.com>
2023-09-14 19:26:17 -0700

    Update SPTextSequence.cs

--- Runtime/UI/Scripts/SPTextSequence.cs
+++ Runtime/UI/Scripts/SPTextSequence.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 
 public class SPTextSequence : MonoBehaviour
 {
-    
+    public System.Action OnUpdated;
+    public System.Action OnDone;
+    public System.Action OnNext;
+    public System.Action OnPrevious;
+
+    public int Index {get{return index;}}
+    public List<SPTextScriptable> Texts {get{return texts;}}
+
     [Header("Text Sequence")]
-    public SPRawText rawText;
-    public SPButton next,previous;
-    public List<SPTextScriptable> texts;
-    public int index = 0;
-    public bool isDone = false;
+    [SerializeField] SPRawText rawText;
+    [SerializeField] SPButton next,previous;
+    [SerializeField] List<SPTextScriptable> texts;
+    [SerializeField] int index = 0;
+    [SerializeField] bool isDone = false;
 
     void Start() {
         Set();
     }
+
+    void Update() {
+        if(SPUIBase.CanInput) {
+
+            if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
+                Next();
+            } else if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)){
+                Previous();
+            }
+        }
+    }
     public void Next() {
         index = Mathf.Clamp(index + 1, 0, texts.Count-1);
         Set();
+        OnNext?.Invoke();
     }
 
     public void Previous() {
         index = Mathf.Clamp(index - 1, 0, texts.Count-1);
         Set();
+        OnPrevious?.Invoke();
     }
 
     void Set() {
@@ -30,5 +50,12 @@ public class SPTextSequence : MonoBehaviour
         isDone = index >= texts.Count-1;
         next?.ToggleWindow(!isDone);
         previous?.ToggleWindow(index > 0);
+
+
+        if(isDone)
+            OnDone?.Invoke();
+
+        OnUpdated?.Invoke();
+        
     }
 }
