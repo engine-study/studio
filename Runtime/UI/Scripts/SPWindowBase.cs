8532bd8a7032662c6f1d3dc0ab551c2d95cec6ee
Neilson <neilson.ks@gmail.com>
2023-09-20 17:17:48 -0700

    Update SPWindowBase.cs

--- Runtime/UI/Scripts/SPWindowBase.cs
+++ Runtime/UI/Scripts/SPWindowBase.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SPWindowBase : SPWindow
 {
-    [Header("Entity")]
+    [Header("SPBase")]
     public SPBase baseObject;
     public SPWindowPosition position;
 
@@ -16,10 +16,7 @@ public class SPWindowBase : SPWindow
     public virtual void UpdateObject(SPBase newBase) {
         baseObject = newBase;
         if(baseObject) {
-            if(position) {
-                position.SetFollow(baseObject.transform);   
-            }
-
+            position?.SetFollow(baseObject.transform);   
         } else {
 
         }
