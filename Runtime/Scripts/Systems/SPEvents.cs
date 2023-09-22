009854713bd43a3983c36d4e1fd59d7b1e596961
Neilson <neilson.ks@gmail.com>
2023-09-21 17:51:38 -0700

    Update SPEvents.cs

--- Runtime/Scripts/Systems/SPEvents.cs
+++ Runtime/Scripts/Systems/SPEvents.cs
@@ -3,5 +3,6 @@ public class SPEvents
 {
     public static Action OnLocalPlayerSpawn;
     public static Action OnServerLoaded;    
+    public static Action OnGameReady;
     public static Action OnGameStarted;
 }
