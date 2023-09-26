80aca1dcb98c03882751b85c99344c1b761be2ad
Neilson <neilson.ks@gmail.com>
2023-09-26 14:51:30 -0700

    text wrap

--- Runtime/Scripts/Interaction/SPMouseRotate.cs
+++ Runtime/Scripts/Interaction/SPMouseRotate.cs
@@ -4,25 +4,41 @@ using UnityEngine;
 
 public class SPMouseRotate : MonoBehaviour
 {
-    public bool useMomentum = false;
-    public bool momentumShift = false;
     public Vector3 Momentum {get{return momentum;}}
     public Vector3 Delta {get{return rot-lastRot;}}
+
+    [Header("Rotate")]
+    public bool onClick = false;
     [SerializeField] float horizontalSpeed = 2.0f;
     [SerializeField] float verticalSpeed = 2.0f;
+    
+    [Header("Momentum")]
+    public bool useMomentum = false;
     [SerializeField] float drag = 1.0f;
+
+    [Header("Shake")]
+    public bool momentumShift = false;
     [SerializeField] float shiftAmount = 1.0f;
     [SerializeField] float shiftMagnitude = 1.0f;
+
     Vector3 rot = new Vector3();
     Vector3 lastRot = new Vector3();
     Vector3 momentum = new Vector3();
     Vector3 startPos;
+
     void Update() {
+
+
 
         lastRot = rot;
 
         float h = horizontalSpeed * -Input.GetAxis("Mouse X") * Time.deltaTime;
         float v = verticalSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime;
+
+        if(!onClick || (onClick && Input.GetMouseButton(0))) {
+            h = horizontalSpeed * -Input.GetAxis("Mouse X") * Time.deltaTime;
+            v = verticalSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime;
+        }
 
         rot = Vector3.up * h + Vector3.right * v;
 
