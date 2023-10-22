d5e6750d04a77c8cf02ced744d8bff3907466d11
Neilson <neilson.ks@gmail.com>
2023-10-22 12:01:43 -0700

    Update SPLine.cs

--- Runtime/Scripts/Util/SPLine.cs
+++ Runtime/Scripts/Util/SPLine.cs
@@ -10,7 +10,9 @@ public class SPLine : MonoBehaviour
     [Header("Debug")]
     [SerializeField] Transform start;
     [SerializeField] Transform target;
-
+    [SerializeField] Vector3 startPos;
+    [SerializeField] Vector3 targetPos;
+    bool targets = false;
     public void Toggle(bool toggle) {
         enabled = toggle;
         Line.enabled = toggle;
@@ -19,10 +21,31 @@ public class SPLine : MonoBehaviour
     public void SetTarget(Transform from, Transform to) {
         start = from;
         target = to;
+
+        if(!from || !to) {Debug.LogError("Null from or to", this);}
+        targets = from && to;
+    }
+
+    public void SetTarget(Vector3 from, Vector3 to) {
+        startPos = from;
+        targetPos = to;
+        targets = false;
     }
 
     void LateUpdate() {
-        Vector3[] positions = new Vector3[] { start.position + Vector3.up * .1f, target.position + Vector3.up * .1f, target.position };
+
+        Vector3 from;
+        Vector3 to;
+
+        if(targets) {
+            from = start.position;
+            to = target.position;
+        } else {
+            from = startPos;
+            to = targetPos;
+        }
+
+        Vector3[] positions = new Vector3[] { from + Vector3.up * .25f, to + Vector3.up * .25f, to };
         Line.SetPositions(positions);
     }
 }
