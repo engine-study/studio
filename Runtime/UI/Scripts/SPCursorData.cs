8f2062e9446d889b26a16ae2cba0aa9d1f24276c
Neilson <neilson.ks@gmail.com>
2023-10-23 10:49:16 -0700

    new pointer

--- Runtime/UI/Scripts/SPCursorData.cs
+++ Runtime/UI/Scripts/SPCursorData.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public enum SPCursorState {Default, Text, Disabled, Hand, Move, TextSelect, Add, Drag, Drop, HandCanDrag, Pointer, PointerSlot, Action, LeftClick, RightClick}
+public enum SPCursorState {Default, Text, Disabled, Hand, Move, TextSelect, Add, Drag, Drop, HandCanDrag, Pointer, PointerSlot, Action, LeftClick, RightClick, HandAction}
 [CreateAssetMenu(fileName = "CursorTextures", menuName = "Engine/UI/CursorTextures", order = 1)]
 public class SPCursorData : ScriptableObject
 {
+    [EnumNamedArray( typeof(SPCursorState) )]
     [SerializeField] public Texture2D [] cursorTextures;
+    [EnumNamedArray( typeof(SPCursorState) )]
     [SerializeField] public Vector2 [] cursorHotspots;
 }
